9e74c370ef2ec0014cb970cd3ae3310aabb545d8
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-27 00:31:48 -0400

    upgrade nxt config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
+ 
   module.exports = {
     images: {
       domains: ['assets.localhost:3000'],
