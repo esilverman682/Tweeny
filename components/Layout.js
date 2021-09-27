8989228281087052574f4990c54fac49d90d7db2
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-27 00:15:14 -0400

    upgrade tailwindcss

--- components/Layout.js
+++ components/Layout.js
@@ -33,7 +33,7 @@ const Header = () => {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">tailstore</span>
+            <span className="ml-3 text-xl">Snappytoes</span>
           </a>
         </Link>
         <nav className="flex flex-wrap items-center justify-center space-x-4 text-base md:ml-auto">
