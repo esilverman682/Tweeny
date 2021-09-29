5ab1b2c51859e0814eec2b2c9e8d1dc6a03b87ca
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-28 20:09:29 -0400

    Dynamic Import added

--- pages/mostComponents.js
+++ pages/mostComponents.js
@@ -5,29 +5,7 @@ import dynamic from "next/dynamic";
 import useInView from "react-cool-inview";
 
 import Hero from "../components/Hero";
-{{/*
-  import HeroRound from "../components/HeroRound"
-  import HeroFull from "../components/HeroFull"
-  import HeroHalf from "../components/HeroHalf"
-  import HeroGradientText from "../components/HeroGradientText"
-  import HeroThreeImages from "../components/HeroThreeImages"
-  import Cta from "../components/Cta"
-  import CtaTwo from "../components/CtaTwo"
-  import HeroFullNoButton from "../components/HeroFullNoButton"
-  import HeroFullThin from "../components/HeroFullThin"
-  import HeroHalfTwo from "../components/HeroHalfTwo"
-  import TitleCards from "../components/TitleCards"
-  import CardImage from "../components/CardImage"
-  import Form from "../components/Form"
-  import TitleCardsTwo from "../components/TitleCardsTwo"
-  import Guardians from "../components/Guardians"
-  import ImageReveal from '../components/ImageReveal'
-  import FooterImage from '../components/FooterImage'
-  import FooterIcons from '../components/FooterIcons'
-  import FooterGradient from '../components/FooterGradient'
-  import FooterForm from '../components/FooterForm'
-  */
-}}
+ 
 const HeroRound = dynamic(() => import("../components/HeroRound"));
 const HeroFull = dynamic(() => import("../components/HeroFull"));
 const HeroHalf = dynamic(() => import("../components/HeroHalf"));
@@ -160,9 +138,16 @@ export default function IndexPage({ isFirstMount }) {
       {isFirstMount && <InitialTransition />}
  
     <Hero></Hero>
-  <ImageReveal></ImageReveal>
- 
+  
+  <motion.h1 variants={title} className="text-6xl font-black text-center h-screen">
+          Welcome to tailstore!
+        </motion.h1>
 
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroRound />}
+    </div>
+   
       <motion.div
         initial="initial"
         animate="animate"
@@ -178,21 +163,70 @@ export default function IndexPage({ isFirstMount }) {
                 <Product key={index} {...product} />
               ))}
             </div>
-
-       <motion.h1 variants={title} className="text-6xl font-black text-center  ">
-          Welcome to tailstore!
-        </motion.h1>
+            <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroFull />}
+    </div>
     <div ref={observe}>
       {/* comments will load when inView is true */}
-      {inView && <Cta />}
+      {inView && <HeroHalf />}
+    </div> 
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroFullNoButton />}
     </div>
- 
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroFullThin />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroGradientText />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <HeroThreeImages />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <Form />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <TitleCards />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <TitleCardsTwo />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <CardImage />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <Guardians />}
+    </div>
     <div ref={observe}>
       {/* comments will load when inView is true */}
       {inView && <FooterForm />}
     </div>
- 
-
+      <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <FooterGradient />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <FooterImage />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <FooterIcons />}
+    </div>
+    <div ref={observe}>
+      {/* comments will load when inView is true */}
+      {inView && <ImageReveal />}
+    </div>
           </div>
         </motion.section>
       </motion.div>
