4d46c57c6845017857de20c6fbdeb5dad3b5e24d
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-10-04 17:35:41 -0400

    Get Started

--- pages/shimmer.js
+++ pages/shimmer.js
@@ -1,32 +1,69 @@
-import * as React from "react";
-import { useState, useRef } from "react";
-import { motion, useDomEvent } from "framer-motion";
-
-const transition = {
-  type: "spring",
-  damping: 25,
-  stiffness: 120
+import { motion } from "framer-motion";
+ 
+import React from "react";
+ import New from '../components/new'
+const content = {
+  animate: {
+    transition: { staggerChildren: 0.1 },
+  },
 };
 
-export const Image = () => {
-  const [isOpen, setOpen] = useState(false);
+const title = {
+  initial: { y: -20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+};
 
-  useDomEvent(useRef(window), "scroll", () => isOpen && setOpen(false));
+const inputs = {
+  initial: { y: -20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+};
 
+const ContactPage = () => {
   return (
-    <div className={`image-container ${isOpen ? "open" : ""}`}>
+ 
+    <motion.section
+      exit={{ opacity: 0 }}
+      className="relative text-gray-700 body-font"
+    >
+
       <motion.div
-        animate={{ opacity: isOpen ? 1 : 0 }}
-        transition={transition}
-        className="shade"
-        onClick={() => setOpen(false)}
-      />
-      <motion.img
-        src="bimhuis.jpg"
-        alt="Bimhuis in Amsterdam"
-        onClick={() => setOpen(!isOpen)}
-        layoutTransition={transition}
-      />
-    </div>
+        variants={content}
+        animate="animate"
+        initial="initial"
+        className="container px-5 py-24 mx-auto"
+      >
+   
+        <motion.div
+          variants={title}
+          className="flex flex-col w-full mb-12 text-center"
+        >
+          <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font">
+            Contact Us
+          </h1>
+          <p className="mx-auto text-base leading-relaxed lg:w-2/3">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify.
+          </p>
+          <New></New>
+        </motion.div>
+
+      </motion.div>
+    </motion.section>
   );
 };
+
+export default ContactPage;
