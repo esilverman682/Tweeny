1088f69172d6fd2f85d7c123d1da3d3cde38dd6e
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-10-05 11:06:26 -0400

    Dynamic import Added

--- pages/index.js
+++ pages/index.js
@@ -188,8 +188,8 @@ export default function IndexPage({ isFirstMount }) {
           className="h-56 w-full object-cover md:h-96 lg:w-full lg:h-3 sm:h-full"
           src="/bruce-mars-FWVMhUa_wbY-unsplash.webp" 
           layout='responsive'
-        width="1200"
-        height="600"  
+        width="1920"
+        height="1280"  
         alt="user profile image"  
           //src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
         />
