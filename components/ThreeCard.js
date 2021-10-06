b5ddc2f62038c3ab7c703d9805bfe147adfc7e33
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-10-05 20:07:30 -0400

    Image widths5

--- components/ThreeCard.js
+++ components/ThreeCard.js
@@ -48,7 +48,7 @@ export default function ThreeCard() {
       height="100" 
 
   quality={100}
-           sizes={[320, 520]}"  
+           sizes={[320, 520]} 
         alt="user profile image"  
         //src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
       />
