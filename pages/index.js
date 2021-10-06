52d34f9084aab080e9877dd3b691ea37ba0cddce
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-10-06 10:54:05 -0400

    section height added

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ export default function IndexPage({ isFirstMount }) {
   return (
     <motion.section exit={{ opacity: 0 }}>
       {isFirstMount && <InitialTransition />}
-      <div className="relative bg-white overflow-hidden">
+      <div className="relative bg-white overflow-hidden" style={{minHeight: "200vh;"}}>
         <div className="max-w-6xl mx-auto">
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg
